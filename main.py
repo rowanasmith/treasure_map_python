bc9e4f76b503229dcb8ed32e5d8fb4afcef66395
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-02 06:43:19 +0000

    Got rid of conditional statements and made the code simpler. It still worked the previous way but this is much better

--- main.py
+++ main.py
@@ -9,15 +9,10 @@ position = input("Where do you want to put the treasure? ")
 
 #Write your code below this row 👇
 
-column = int(position[0]) - 1
+column = int(position[0])
 row = int(position[1])
 
-if row == 1:
-    row1[column] = "X"
-elif row == 2:
-    row2[column] = "X"
-else:
-    row3[column] = "X"
+map [row-1] [column-1] = "X"
 
 #Write your code above this row 👆
 
